e7f6623f32bc4bc776b0d1db0f7a30cd50ac40ac
yaaa3ser <91497279+yaaa3ser@users.noreply.github.com>
2022-10-20 15:55:28 +0200

    Update settings.py

--- poetry_school/school/settings.py
+++ poetry_school/school/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jd%&%n42@!(@0c18i))zd*evea!*am*k^7-w8lu@)fjn=n+5o4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
